bbdffc774a227526bebe386e5ec3a4a313d522cc
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-18 14:35:04 +0500

    Update writer.java

--- writer.java
+++ writer.java
@@ -1,12 +1,15 @@
 import java.io.FileWriter;  
-public class FileWriterExample {  
+public class writer {  
     public static void main(String args[]){ 
        try{    
 
            FileWriter fw = new FileWriter("C:/Users/MFY/Desktop/I-LOVE-GIT-COMMITS/file.txt");    
            fw.write("Welcome to javaTpoint.");    
+           
+           
+           
            fw.close();    
-          }catch(Exception e){System.out.println(e);}    
-          System.out.println("Success...");    
-     }    
+          }
+          catch(Exception e){System.out.println(e);}    
+    }    
 }  
