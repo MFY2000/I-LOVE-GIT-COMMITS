9e73c58d26bacb2d40c0b1fec14a695a7dbe5c70
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-18 14:42:32 +0500

    Update writer.java

--- writer.java
+++ writer.java
@@ -5,6 +5,17 @@ public class writer {
 
            FileWriter fw = new FileWriter("C:/Users/MFY/Desktop/I-LOVE-GIT-COMMITS/file.txt");    
            
+           String fileName = "/Users/pankaj/source.txt";
+           File file = new File(fileName);
+           FileReader fr = new FileReader(file);
+           BufferedReader br = new BufferedReader(fr);
+           String line;
+           while((line = br.readLine()) != null){
+               //process the line
+               System.out.println(line);
+           }
+
+
            String message = "4";
            fw.write(message);    
            
