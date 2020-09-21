4ea1018783121d6ab51de57ba8667b6a7f97bebf
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-09-21 15:07:32 +0500

    git add --all

--- writer.java
+++ writer.java
@@ -2,15 +2,12 @@ import java.io.FileWriter;
 public class writer {  
     public static void main(String args[]){ 
        try{    
-
-           FileWriter fw = new FileWriter("C:/Users/MFY/Desktop/I-LOVE-GIT-COMMITS/file.txt");    
-          
-           char message = (char) ((int) ((Math.random()*26)+65));
-           fw.write(message);    
-           
-           
-           
-           fw.close();    
+            for (int i = 0; i < 10; i++) {
+            FileWriter fw = new FileWriter("C:/Users/MFY/Desktop/Fun With Python/Checking/file"+i+".txt");      
+            char message = (char) ((int) ((Math.random()*26)+65));
+            fw.write(message);    
+            fw.close();    
+            }
           }
           catch(Exception e){System.out.println(e);}    
     }    
